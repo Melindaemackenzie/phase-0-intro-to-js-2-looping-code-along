95f088c367e6fb6938899bfba2b03efa60919b75
melindamackenzie <mem167@pitt.edu>
2023-07-26 14:04:27 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (cards,event) {
+    let messagesArray = []
+    for (let i =0; i < cards.length; i++) {
+       messagesArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`) 
+       console.log(messagesArray)
+    }
+    return messagesArray;
+    }
+
+  
+
+        function countDown(a) {
+            while (a >=0)
+            console.log(a--);
+        }
+    
